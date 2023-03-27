eea0df4a37014e647ffc0c316b17b33d986e24a6
Purinat33 <Purinat33@gmail.com>
2023-03-28 01:34:14 +0700

    Testing SQL transaction for multiple queries in 1 function

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -104,22 +104,33 @@ const deleteUser = async (req, res, next) => {
   const userId = req.params.id;
 
   // Use the user ID to delete the corresponding cart records from the database
-  try {
-    console.log(`Deleting cart records for user ${userId}`);
-    await db.promise().query('DELETE FROM cart WHERE uid = ?', [userId]);
-  } catch (error) {
-    console.log(error);
-    res.status(500).render('error', {message: 'Internal server error'});
-    return;
-  }
+  try{
+    await db.promise().query('START TRANSACTION');
+    try {
+      console.log(`Deleting cart records for user ${userId}`);
+      await db.promise().query('DELETE FROM cart WHERE uid = ?', [userId]);
+    } catch (error) {
+      console.log(error);
+      await db.promise().query('ROLLBACK');
+      res.status(500).render('error', {message: 'Internal server error'});
+      return;
+    }
 
-  // Use the user ID to delete the corresponding user from the database
-  try {
-    console.log(`Deleting user ${userId}`);
-    await db.promise().query('DELETE FROM users WHERE id = ?', [userId]);
-    res.status(200).render('success', {message: "Successfully delete user from the database", token: req.cookies.token});
-  } catch (error) {
-    console.log(error);
+    // Use the user ID to delete the corresponding user from the database
+    try {
+      console.log(`Deleting user ${userId}`);
+      await db.promise().query('DELETE FROM users WHERE id = ?', [userId]);
+      res.status(200).render('success', {message: "Successfully delete user from the database", token: req.cookies.token});
+    } catch (error) {
+      console.log(error);
+      await db.promise().query('ROLLBACK');
+      res.status(500).render('error', {message: 'Internal server error'});
+      return;
+    }
+
+  }catch(err){
+    console.log(err);
+    await db.promise().query('ROLLBACK');
     res.status(500).render('error', {message: 'Internal server error'});
     return;
   }
